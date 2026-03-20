b83f6f7bfb47af8651490af073719f7704af1c3b
Agustin <adreyfus@miuandes.cl>
2026-03-20 10:30:53 -0300

    Agregar clases Tarea y Recurso

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ import sys
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict, List, Set, Tuple
+
+#Tarea guarda el identificador y la duración
+#Recurso guarda el identificador y cuándo queda libre
+#@dataclass hace más fácil crear objetos sin escribir tanto código
+
+@dataclass
+class Tarea:
+    id: str
+    duracion: int
+
+
+@dataclass
+class Recurso:
+    id: str
+    disponible: int = 0
